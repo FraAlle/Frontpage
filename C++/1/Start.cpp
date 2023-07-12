ab7cf3b1d130b6e07439759e83948750010839a8
Francesco Allegrini <franciwwf@gmail.com>
2023-07-12 17:57:49 +0200

    C++/

--- C++/1/Start.cpp
+++ C++/1/Start.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
-using namespace std;
-
-int main() 
-{
-  cout << "It's me, your first program.";
-  return 0;
+using namespace std;    //abstract container hold logical unique entities / using namespace is a C++ standard
+                        //it's like import functions in python, or u import the namespace generally o u must call it in this way std::cout
+                        //std é un oggetto
+void pippo(){
+  cout << "Hello World";
 }
+
+int main(void)
+{
+  pippo();
+  return 1;             //interrupt the function when the code return something
+}
+
