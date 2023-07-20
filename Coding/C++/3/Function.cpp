cb67fcc9d411599c49648d3d641798413379c901
Francesco Allegrini <franciwwf@gmail.com>
2023-07-20 02:58:17 +0200

    Coding/

--- Coding/C++/3/Function.cpp
+++ Coding/C++/3/Function.cpp
@@ -4,6 +4,37 @@
 
 using namespace std;
 
+float square(float,float);        //necessary if we want to put the function after the main
+            //this has to be the number of variable we pass to the function wich each different type
+//thi is the return type
+void fun();
+
+
+void by_ptr(int*);
+
+
+int globalVar = 0;              //this is a global variable
 int main(){
-    
+    float y =square(10.1,20.);
+    cout<<y<<endl;
+
+    int variable = 1;
+    int *pointer = &variable;   //take the pointer position of variable
+    by_ptr(pointer);            //pass the pointer
+    cout<<"variable: "<<variable<<endl; 
+
+}
+
+float square(float x,float z){
+    x = x*x;
+    return x;
+}
+
+void fun(){
+
+}
+
+void by_ptr(int* ptr) 
+{
+	*ptr = *ptr + 1;        //add +1 a the pointer where variable is located
 }
--- Coding/C++/3/functions.md
+++ Coding/C++/3/functions.md
@@ -1,1 +1,4 @@
 #FUNCTIONS
+parameters passed by refer can be changed inside the functions.
+U can use functions with pointers, in this case u will perform an parameter by value but it will be changeble bc we work with the posistion of the memory of that variable.
+u can omit the parameters that are sets in the function definition. f.e. void new_greet(string greet, int repeats = 1) when the function in called there is no need to insert both values bc the second is already set as 1.
